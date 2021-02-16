8df4f7cd8d9c9b8741ab95483c6f28d363e826a3
jpeden <jpeden@gmail.com>
2021-02-15 19:24:19 -0500

    updated repo

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 //go:generate swagger generate model --accept-definitions-only -f api/swagger.yml -t ./generated
 package main
 
-import "github.com/jake-scott/smartthings-nest/cmd"
+import "github.com/jpeden/smartthings-nest/cmd"
 
 func main() {
 	cmd.Execute()
